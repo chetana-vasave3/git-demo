033a5f6f52794be7631004861bb8983006bbcb06
chetana-vasave3 <chetanavasave3@gmail.com>
2025-01-21 15:20:04 +0530

    adding file

---
+++ main.py
@@ -0,0 +1,20 @@
+# def add(a,b):
+#     print(a+b)
+
+
+
+def hello():
+    print("Hello Chetana")
+# hello()
+
+  
+def even(n):
+    if n%2==0:
+        print(n,"even")
+    else:
+        print(n,"odd")
+# even(50)
+
+if __name__== "__main__":
+     hello()
+     
